8a961ac23eab03cb892854565024170c1bc6180f
petrosenyo <petrosenyo@gmail.com>
2025-08-12 20:00:48 +0300

    change config vite.config.js & docker-compose.yaml

--- src/vite.config.js
+++ src/vite.config.js
@@ -10,4 +10,18 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        // усередині контейнера Vite слухає всі інтерфейси
+        host: true,        // еквівалент --host
+        port: 5173,
+        strictPort: true,
+        cors: true,        // дозволяє крос-домени з :8000
+        origin: 'http://localhost:5173', // що підставлятиметься у <script href=...>
+        hmr: {
+            host: 'localhost', // куди підключається клієнт HMR із браузера на Windows
+            port: 5173,
+            protocol: 'ws',
+            clientPort: 5173,  // важливо для Windows/WSL
+        },
+    },
 });
